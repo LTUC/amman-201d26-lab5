e3d012381af86f03347901914d30bc0a70b9acc2
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-05-27 14:21:35 +0300

    adding problem 4 to github

--- app.js
+++ app.js
@@ -73,7 +73,9 @@ function sumAndMultiply(a, b, c) {
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument 
+and then returns an array where the first element is the sum of the numbers in the array, 
+and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
@@ -85,13 +87,18 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
+  let sumTwo = sum(sumArr[0], sumArr[1])[0];
+  let resultOfSum = sum(sumTwo, sumArr[2])[0];
+  // console.log(resultOfSum);
+  return [resultOfSum, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${resultOfSum} is their sum.`];
 
   //eslint-disable-line
 }
+//  sumArray(testArray);
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
