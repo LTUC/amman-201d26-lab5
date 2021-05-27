02a4af1cc82bc2146dd646725d6cf852715b948d
Mohammad Harb <md20.harb21@gmail.com>
2021-05-27 14:20:14 +0300

    porb3 has solved

--- app.js
+++ app.js
@@ -27,16 +27,15 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-// function multiply(a, b) {
-//   //eslint-disable-line
-//   let answer=a*b;
-//   return[answer,`The product of ${a} and ${b} is ${answer}.`]
+function multiply(a, b) {
+  //eslint-disable-line
+  let answer=a*b;
+  return[answer,`The product of ${a} and ${b} is ${answer}.`]
 
-  
-// }
+}
 
-// // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+// Here is the test for multiply(); uncomment it to run it
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -54,12 +53,17 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
   //eslint-disable-line
+let sumCall= sum(a,b)[0];
+let sumAnswer= sum(sumCall,c)[0];
+let multiplyCall= multiply(a,b)[0];
+let multiplyAnswer= multiply(multiplyCall,c)[0];
 
-  
+return [sumAnswer,multiplyAnswer, `${a} and ${b} and ${c} sum to ${sumAnswer}.`,`The product of ${a} and ${b} and ${c} is ${multiplyAnswer}.`]
+ 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
