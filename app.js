e664851a00cce15f270b4d87941b96973ef5e986
Raghad-Abdelghani <raghadabdelghani@gmail.com>
2021-05-30 03:08:13 +0300

    solved problem5

--- app.js
+++ app.js
@@ -81,16 +81,14 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(arr) {
-  let sum = 0;
-  for (let val of arr) {
-    sum += val;
+  function sumArray(arr) {
+    let sumOne= sum(2,3)[0]
+    let sumAll= sum(sumOne,4)[0]
+  
+    return[sumAll,`${2},${3},${4} was passed in as an array of numbers, and ${9} is their sum.`
+  ]
   }
   
-
-  return[sum,`${2},${3},${4} was passed in as an array of numbers, and ${9} is their sum.`
-]
-}
 
 // Here is the test for sumArray(); uncomment it to run it
 
@@ -109,14 +107,22 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+
+let testArray2= [2, 3, 4];
 function multiplyArray(multArr) {
   
+    let multyOne= multiply(2,3)[0]
+    let multyAll= multiply(multyOne,4)[0]
+  
+    return[multyAll,`The numbers ${2},${3},${4} have a product of ${24}.`]
+
+  }
   //eslint-disable-line
 
-}
+
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
